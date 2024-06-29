3bf87a5577345577b78685c1a5c91aa1c8cd7b5b
imVey <marwanbella91@hotmail.fr>
2024-06-29 17:02:23 +0200

    Feat: Add Projects page

---
+++ app/projects/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <h1>Projects</h1>
+      <p>
+        Coming soon... mais ici vous pourrez retrouver mes projets réalisé et
+        les projets à venir.
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
