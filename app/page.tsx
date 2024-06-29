37137971c4148442e0fcbb6e28eb6ec50eab23a5
imVey <marwanbella91@hotmail.fr>
2024-06-29 18:09:18 +0200

    Feat: Add link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import React from "react";
+import Intro from "./intro/page";
+import Presentation from "./presentation/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Intro />
+      <Presentation />
+    </main>
   );
 }
