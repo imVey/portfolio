57efa5fcdb9f20848352653710760de28d09ea26
imVey <marwanbella91@hotmail.fr>
2024-06-29 17:02:10 +0200

    Feat: Add Contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <p>Ceci sera la page de prise de contact. Coming soon...</p>
+    </div>
+  );
+};
+
+export default Contact;
