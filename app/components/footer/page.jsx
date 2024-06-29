1a98d07187781571a1e3cdd375a0ac20b795790c
imVey <marwanbella91@hotmail.fr>
2024-06-29 17:01:46 +0200

    Fix: Rename all the files to let all the route work again

---
+++ app/components/footer/page.jsx
@@ -0,0 +1,28 @@
+const Footer = () => {
+  return (
+    <footer className="footer bg-transparent pb-5 ">
+      <div className="flex gap-52 mt-10 mb-20  ml-80">
+        <section className="flex flex-col items-start mt-10">
+          <a href="/terms-of-service">Terms of Service </a>
+          <a href="/privacy-policy">Privacy Policy </a>
+          <a href="/cookie-policy">Cookie Policy </a>
+        </section>
+        <section className="flex flex-col items-start mt-10">
+          <a href="/sitemap.xml">Site Map </a>
+          <a href="/accessibility-statement">Accessibility Statement </a>
+          <a href="/contact">Contact </a>
+        </section>
+        <section className="flex flex-col items-start mt-10">
+          <a href="https://github.com/imVey">GitHub </a>
+          <a href="https://www.linkedin.com/in/bellamarwan/">LinkedIn </a>
+          <a href="https://twitter.com/">Twitter </a>
+        </section>
+      </div>
+      <p className="mt-10 flex flex-col items-center">
+        &copy; {new Date().getFullYear()} My Portfolio App. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/components/header/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <img className=" w-10 object-contain" src="/mb.png" alt="Logo" />
+          </li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projets</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
